cc324e8558b2cd507d7a38cad43187ff94b2d62a
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-06 13:35:50 +0200

    ADD task 2 and 3

---
+++ OS/OS - 2/Task.2/Task.2.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int* filter_even(int* arr, int n, int* count) {
+	int even_count = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0) {
+			even_count++;
+		}
+	}
+
+	*count = even_count;
+	int* even_arr = (int*)malloc(even_count * sizeof(int));
+
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0) {
+			even_arr[j] = arr[i];
+			j++;
+		}
+	}
+
+	return even_arr;
+}
+
+int main() {
+	int n = 4;
+	int arr[] = {1, 2, 3, 4};
+	int countEven = 0;
+
+	int* evens = filter_even(arr, n, &countEven);
+
+	for (int i = 0; i < countEven; i++) {
+		printf("%d \n", evens[i]);
+	}
+
+	if (evens != NULL) {
+		free(evens);
+	}
+	return 0;
+}
---
+++ OS/OS - 2/Task3/Task3.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int* find(int* arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) { 
+            return &arr[i];
+        }
+    }
+    return NULL; 
+}
+
+int main() {
+    int numberOfRead, x;
+
+    printf("Input number of read: ");
+    scanf_s("%d", &numberOfRead);
+
+    int* arr = (int*)malloc(numberOfRead * sizeof(int));
+    if (arr == NULL) return 1;
+
+    printf("Input %d numbers:\n", numberOfRead);
+    for (int i = 0; i < numberOfRead; i++) {
+        scanf_s("%d", &arr[i]);
+    }
+
+    printf("Input searched number: ");
+    scanf_s("%d", &x);
+
+    int* foundX = find(arr, numberOfRead, x);
+
+    if (foundX != NULL) {
+        int position = (int)(foundX - arr);
+        printf("Намерено: %d на позиция (индекс): %d\n", *foundX, position);
+        printf("Адрес в паметта: %p\n", (void*)foundX);
+    }
+    else {
+        printf("Числото не е намерено.\n");
+    }
+
+    free(arr); 
+    return 0;
+}
