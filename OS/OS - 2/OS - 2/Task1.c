ad5d8bb566714472f9bf00fd2ae946456b927dfa
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-06 13:02:44 +0200

    add task 2

---
+++ OS/OS - 2/OS - 2/Task1.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int main() {
+
+    printf("\n=== Dynamic Array ===\n");
+    int n;
+    printf("Inpunt n: ");
+    scanf_s("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+    }
+
+    printf("Elements: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    free(arr);
+    arr = NULL;
+
+    return 0;
+}
