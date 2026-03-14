fb6e7ca7d69d510f9b13195df3e2abc9889aa81c
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-14 14:21:23 +0200

    add dynamic data into ejs file

--- Internet Based Technologies/ExpressApps/server/app.js
+++ Internet Based Technologies/ExpressApps/server/app.js
@@ -2,19 +2,32 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+
+    const blogs = [
+        {title: "Blog 1", snippet: 'This is blog 1'},
+        {title: "Blog 2", snippet: 'This is blog 2'},
+        {title: "Blog 3", snippet: 'This is blog 3'},
+    ];
+
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', { title: 'About Us' });
 });
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
+});
+
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
-})
+    res.status(404).render('404', { title: '404' });
+});
 app.listen(3000);
