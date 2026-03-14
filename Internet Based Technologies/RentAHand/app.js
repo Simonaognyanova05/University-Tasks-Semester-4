e3e8fbe4878244dda8373ec0fcaa2c6197ac5858
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-14 20:51:38 +0200

    add views in project

---
+++ Internet Based Technologies/RentAHand/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home page' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About page' });
+});
+
+app.get('/catalog', (req, res) => {
+    res.render('catalog', { title: 'Catalog page' });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact page' });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login page' });
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', { title: 'Register page' });
+});
+
+app.get('/upload', (req, res) => {
+    res.render('upload', { title: 'Upload page' });
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: 'Not found' });
+})
