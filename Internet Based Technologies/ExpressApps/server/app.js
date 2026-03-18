51850178775b12480002c0e0cd41518ffeb5305b
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-18 18:17:05 +0200

    add relation to mongoDB

--- Internet Based Technologies/ExpressApps/server/app.js
+++ Internet Based Technologies/ExpressApps/server/app.js
@@ -1,25 +1,72 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Service = require('./models/service');
 
 const app = express();
+
+const dbURL = 'mongodb+srv://SimonaOgnyanova:123Simona2005@rent-hand.fcxtivy.mongodb.net/';
+mongoose.connect(dbURL)
+    .then(() => {
+        app.listen(3000);
+    }).catch((err) => {
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
 app.use((req, res, next) => {
     console.log('host: ', req.hostname);
     next();
+});
+
+app.get('/add-service', (req, res) => {
+    const service = new Service({
+        title: 'Математика',
+        description: 'Уроци по интеграли',
+        location: 'Пловдив',
+        price: 25,
+    });
+
+    service.save()
+        .then((result) => res.send(result))
+        .catch(err => console.log(err));
+});
+
+app.get('/all-services', (req, res) => {
+    Service.find()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+app.get('/single-service', (req, res) => {
+    Service.findById('69bacb511f0b99b5d5cbe292')
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
 app.get('/', (req, res) => {
-
-    const blogs = [
-        { title: "Blog 1", snippet: 'This is blog 1' },
-        { title: "Blog 2", snippet: 'This is blog 2' },
-        { title: "Blog 3", snippet: 'This is blog 3' },
-    ];
-
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/services');
 });
+
+app.get('/services', (req, res) => {
+    Service.find().sort({createdAt: -1})
+    .then(result => {
+        res.render('index', {title: 'Services', services: result});
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About Us' });
@@ -36,4 +83,3 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
-app.listen(3000);
